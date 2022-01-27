8d249925463137b695a423848c18ad2ace9ced42
Jissay <jc.ricard@me.com>
2022-01-27 23:43:09 +0100

    Added new Grass tiles + Camera drag + Mouse cursor on camera drag + Texture loader tool class

---
+++ TATTOT/Assets/Code/ResourcesLoaders/TATextureLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Code.ResourcesLoaders
+{
+    public class TATextureLoader
+    {
+        public static Texture2D CameraMoveCursor => Resources.Load<Texture2D>("Sprites/Camera-move-cursor");
+    }
+}
--- TATTOT/Assets/Code/UI/TAMainCamera.cs
+++ TATTOT/Assets/Code/UI/TAMainCamera.cs
@@ -1,3 +1,4 @@
+using Code.ResourcesLoaders;
 using UnityEngine;
 
 namespace Code.UI
@@ -6,41 +7,54 @@ namespace Code.UI
     {
         #region Movement parameters
 
-        private const int Speed = 100;
+        private const int CameraKeyboardMoveSpeed = 100;
+        private const int CameraDragSpeed = 3;
 
-        private static Vector3 UpDirection => new Vector3(0, Speed * Time.deltaTime, 0);
-        private static Vector3 RightDirection => new Vector3(Speed * Time.deltaTime, 0);
-        private static Vector3 DownDirection => new Vector3(0, -Speed * Time.deltaTime, 0);
-        private static Vector3 LeftDirection => new Vector3(-Speed * Time.deltaTime, 0, 0);
+        private static Vector3 UpDirection => new Vector3(0, CameraKeyboardMoveSpeed * Time.deltaTime, 0);
+        private static Vector3 RightDirection => new Vector3(CameraKeyboardMoveSpeed * Time.deltaTime, 0);
+        private static Vector3 DownDirection => new Vector3(0, -CameraKeyboardMoveSpeed * Time.deltaTime, 0);
+        private static Vector3 LeftDirection => new Vector3(-CameraKeyboardMoveSpeed * Time.deltaTime, 0, 0);
+        
+        private Vector3 _mouseOrigin;
 
         #endregion
 
         #region Zoom parameters
-
+        
+        private const float CameraScrollSensitivity = 100f;
+        private const float CameraScrollMaxZIndex = -15f;
+        
         private float _minZIndex;
-
-        private const float Sensitivity = 100f;
-        private const float MaxZIndex = -15f;
 
         #endregion
 
+        #region MonoBehaviour lifecycle
+
         private void Start()
         {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            
             _minZIndex = transform.position.z;
         }
 
-        private void Update()
+        private void LateUpdate()
         {
-            MoveCameraIfNeeded();
+            MoveCameraOnKeyboardPress();
+            MoveCameraOnMouseDrag();
             ZoomCameraIfNeeded();
         }
-        
+
+        #endregion
+
+        #region Camera movement
+
         /// <summary>
         /// Move the camera if the current key press is one that should be moving the camera.
         /// Multiple if are used as we want the camera to being able to be translated in
         /// multiple directions. That could happen if the user presses two directions at a time.
         /// </summary>
-        private void MoveCameraIfNeeded()
+        private void MoveCameraOnKeyboardPress()
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
@@ -63,17 +77,49 @@ namespace Code.UI
             }
         }
 
+        private void MoveCameraOnMouseDrag()
+        {
+            // Store dragging position 
+            if (Input.GetMouseButtonDown(1))
+            {
+                _mouseOrigin = Input.mousePosition;
+                return;
+            }
+            
+            // Move camera on X & Y while button isn't released
+            if (Camera.main is null || !Input.GetMouseButton(1))
+            {
+                // Reset the cursor to it's initial value
+                Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+                return;
+            }
+
+            // Set cursor to camera move cursor
+            Cursor.SetCursor(TATextureLoader.CameraMoveCursor, Vector2.zero, CursorMode.Auto);
+            
+            // Update x and y axis position, relatively to where the mouse point in the world
+            var pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - _mouseOrigin);
+            var move = new Vector3 (pos.x * CameraDragSpeed, pos.y * CameraDragSpeed, 0);
+            Camera.main.transform.Translate (move, Space.World);
+        }
+
+        #endregion
+
+        #region Camera zoom
+
         /// <summary>
         /// Zoom the camera if the user is scrolling in or out with his mouse wheel.
         /// </summary>
         private void ZoomCameraIfNeeded()
         {
-            var scroll = Input.GetAxis("Mouse ScrollWheel") * Sensitivity;
+            var scroll = Input.GetAxis("Mouse ScrollWheel") * CameraScrollSensitivity;
             if (scroll + transform.position.z >= _minZIndex &&
-                scroll + transform.position.z <= MaxZIndex)
+                scroll + transform.position.z <= CameraScrollMaxZIndex)
             {
                 transform.Translate(0, 0, scroll);
             }
         }
+
+        #endregion
     }
 }
