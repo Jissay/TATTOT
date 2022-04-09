2fa933361e2d6a0a074fe16b6d8c3e5ea9e02794
Jissay <jc.ricard@me.com>
2022-04-10 00:48:02 +0200

    Library Added for TextMeshPro

---
+++ TATTOT/Assets/Code/UI/TAUICanvas.cs
@@ -0,0 +1,40 @@
+using System;
+using Code.Logic.Events;
+using Code.Logic.Terrain;
+using Code.ResourcesLoaders;
+using TMPro;
+using UnityEngine;
+
+namespace Code.UI
+{
+    public class TAUICanvas : MonoBehaviour
+    {
+        public TextMeshProUGUI terrainTierText;
+        public TextMeshProUGUI terrainSlotsText;
+        public TextMeshProUGUI terrainTypeText;
+
+        private void Awake()
+        {
+            gameObject.SetActive(false);
+            TAEventManager.Shared().DidSelectTerrainEvent.AddListener(DidSelectTerrain);
+            TAEventManager.Shared().DidUnselectTerrainEvent.AddListener(DidUnselectTerrain);
+        }
+
+        #region Event handling
+
+        private void DidSelectTerrain(TATerrain terrain)
+        {
+            gameObject.SetActive(true);
+            terrainTierText.text = TATextLoader.Tier + terrain.Tier;
+            terrainSlotsText.text = TATextLoader.Slots + terrain.AvailableSlots;
+            terrainTypeText.text = TATextLoader.Type + terrain.TileName;
+        }
+
+        private void DidUnselectTerrain()
+        {
+            gameObject.SetActive(false);
+        }
+        
+        #endregion
+    }
+}
